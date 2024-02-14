3de9a248cebeffcc78c28e696df8ac9bd8478a43
doa <leedoa0819@gmail.com>
2024-02-14 14:30:23 +0900

    [24.02.13-14]

---
+++ src/main/java/com/ohgiraffers/section02/looping/test.java
@@ -0,0 +1,40 @@
+package com.ohgiraffers.section02.looping;
+
+public class test {
+    public static void main(String[] args) {
+
+ //       for (int i = 1; i <=10; i++){
+
+ //           System.out.println(i);
+
+//        for (int i = 10; i>=1; i--){
+ //           System.out.println(i);
+
+ //       for (int i = 7; i <8; i++){
+
+//            for (int su=1; su<10; su++){
+//
+//                System.out.println(i+" * " + su + " = " + (i*su));
+//            }
+
+//        for (int i =2; i<=100; i++){
+//
+//            if (i % 2 ==0){
+//                System.out.println(i);
+//            }
+//        }
+
+
+
+
+
+        }
+
+
+        }
+
+
+
+
+
+
